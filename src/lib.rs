6c85b590cb21e5d8c1519d4a7e63c377ca86359e
Max-Wurthmann <mx.wurthmann@gmail.com>
2025-05-04 00:29:38 +0200

    extracted alltoallv into lib fn for testing, has to remain in main to be run with  mpiexec context

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use mpi::datatype::{Partition, PartitionMut};
+use mpi::traits::*;
 use mpi::traits::{Communicator, Root};
 
 pub fn sort<'a>(values: &'a [i32], splitter: &[i32]) -> &'a [i32] {
@@ -37,11 +39,53 @@ pub fn sort<'a>(values: &'a [i32], splitter: &[i32]) -> &'a [i32] {
     // sort locally
 }
 
+pub fn mpi_scatter() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let rank = world.rank();
+    let size = world.size();
+
+    let root_rank = 0;
+    let root_process = world.process_at_rank(root_rank);
+
+    let mut recv_val = 0;
+
+    if rank == root_rank {
+        let send_vals: Vec<i32> = (0..size).collect();
+        root_process.scatter_into_root(&send_vals, &mut recv_val);
+    } else {
+        root_process.scatter_into(&mut recv_val);
+    }
+
+    assert_eq!(recv_val, rank);
+}
+
+pub fn mpi_alltoallv() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let rank = world.rank();
+    let size = world.size();
+
+    let counts: Vec<i32> = vec![1; size as usize];
+    let displs: Vec<i32> = (0..size).collect();
+
+    let send_buf: Vec<i32> = (0..size).collect();
+    let send_partition = Partition::new(&send_buf, counts.clone(), displs.clone());
+
+    let mut recv_buf: Vec<i32> = vec![-1; size as usize];
+    let mut recv_partiotion = PartitionMut::new(&mut recv_buf, counts, displs);
+
+    world
+        .any_process()
+        .all_to_all_varcount_into(&send_partition, &mut recv_partiotion);
+
+    assert_eq!(recv_buf, vec![rank; size as usize]);
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::sort;
-    use mpi::traits::*;
     use rand::Rng;
 
     #[test]
@@ -53,28 +97,5 @@ mod tests {
         expected.sort_unstable();
         let result = sort(&values, &[5, 10, 15]);
         assert_eq!(expected, result);
-    }
-
-    #[test]
-    fn mpi_basics() {
-        let world = mpi::initialize().unwrap().world();
-
-        let rank = world.rank();
-        let size = world.size();
-
-        let root_rank = 0;
-        let root_process = world.process_at_rank(root_rank);
-
-        let mut x = 0;
-
-        if rank == root_rank {
-            let v = (0..size).collect::<Vec<_>>();
-
-            root_process.scatter_into_root(&v, &mut x);
-        } else {
-            root_process.scatter_into(&mut x);
-        }
-
-        assert_eq!(x, rank);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 use mpi::datatype::{Partition, PartitionMut};
 use mpi::traits::*;
+use rust_mpi::{mpi_alltoallv, mpi_scatter};
 
 fn main() {
-    // let size = 2;
-    // let counts: Vec<i32> = Vec::new();
-    // let displs: Vec<i32> = Vec::new();
-    //
-    // let mut recv_buf: Vec<i32> = Vec::with_capacity(size as usize);
-    //
-    // let recv_partiotion = PartitionMut::new(&mut recv_buf, &counts[..], &displs[..]);
-    // println!();
-    // println!();
-    // println!("{:?}", recv_partiotion.counts());
-    // todo!();
+    mpi_scatter();
+    mpi_alltoallv();
 
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
@@ -27,20 +19,12 @@ fn main() {
     let send_buf: Vec<i32> = (0..size).collect();
     let send_partition = Partition::new(&send_buf, counts.clone(), displs.clone());
 
-    let mut recv_buf: Vec<i32> = vec![0; size as usize];
+    let mut recv_buf: Vec<i32> = vec![-1; size as usize];
     let mut recv_partiotion = PartitionMut::new(&mut recv_buf, counts, displs);
 
     world
         .any_process()
         .all_to_all_varcount_into(&send_partition, &mut recv_partiotion);
-
-    // let mut recv_buf = vec![0_i32; chunk_len];
-    // if rank == root_rank {
-    //     let send_buf: Vec<i32> = (0..(size * chunk_len as i32)).collect();
-    //     root_process.scatter_into_root(&send_buf, &mut recv_buf);
-    // } else {
-    //     root_process.scatter_into(&mut recv_buf);
-    // }
 
     println!("rank {rank} reports: {:?}", recv_buf);
 }
